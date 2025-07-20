a6b06c0d5eaae863440132e527838ec3b6b3f2a3
EricSax76 <ericmoscoso76@hotmail.com>
2025-07-20 19:40:34 +0200

    20 de julio

--- backend/controllers /candidateController.js
+++ backend/controllers /candidateController.js
@@ -1,20 +1,15 @@
-import { createCandidate, getAllCandidates, getCandidateById } from '../models/candidateModel.js';
 import { createUser } from '../models/userModel.js';
+import { createCandidate, getAllCandidates, getCandidateById } from '../models/candidateModel.js';
 
 export const createCandidateController = async (req, res) => {
   try {
-    const { nombre, email, password, skills, experience } = req.body;
-
-    // 1. Crear el usuario
-    const user = await createUser({ nombre, email, password });
-
-    // 2. Crear el candidato vinculado al usuario
+    const { name, email, password, skills, experience } = req.body;
+    const user = await createUser({ name, email, password });
     const candidate = await createCandidate({
       userId: user.id,
       skills,
-      experience
+      experience,
     });
-
     res.status(201).json({ message: 'Candidate created successfully', candidate });
   } catch (error) {
     console.error(error);
@@ -22,7 +17,7 @@ export const createCandidateController = async (req, res) => {
   }
 };
 
-export const getCandidatesController = async (req, res) => {
+export const getCandidatesController = async (_req, res) => {
   try {
     const candidates = await getAllCandidates();
     res.status(200).json({ message: 'Candidates retrieved successfully', candidates });
@@ -35,9 +30,9 @@ export const getCandidateByIdController = async (req, res) => {
   try {
     const { id } = req.params;
     const candidate = await getCandidateById(id);
-
-    if (!candidate) return res.status(404).json({ message: 'Candidate not found' });
-
+    if (!candidate) {
+      return res.status(404).json({ message: 'Candidate not found' });
+    }
     res.status(200).json({ message: 'Candidate retrieved successfully', candidate });
   } catch (error) {
     res.status(500).json({ message: 'Error retrieving candidate', error });
